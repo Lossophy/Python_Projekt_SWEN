442c410932dffbcae0c2a31a476c3700959e875f
Adrian Siegenthaler <siega7@bfh.ch>
2025-10-14 11:23:36 +0200

    Städtereise erstellt

--- Backend/daten.py
+++ Backend/daten.py
@@ -4,21 +4,34 @@ from backend.modelle import Reise, Kategorie, Gegenstand
 
 def beispielreisen_laden() -> (
     list[Reise]
-):  # Erstellt eine Liste mit Beispielreisen, die beim App-Start angezeigt werden können."""
+):  # Erstellt eine Liste mit Beispielreisen, die beim App-Start angezeigt werden können
     reisen = []
 
     # Beispiel 1 – Städtereise
-    stadt = Reise(
-        "Städtereise", date(2025, 5, 10), date(2025, 5, 14), "Kurztrip in die Stadt"
-    )
+    stadt = Reise("Städtereise", date(2026, 5, 10), date(2026, 5, 14), "Kurztrip")
     kleidung = Kategorie("Kleidung")
     kleidung.gegenstand_hinzufuegen(Gegenstand("Hose", 2))
     kleidung.gegenstand_hinzufuegen(Gegenstand("T-Shirt", 3))
+    kleidung.gegenstand_hinzufuegen(Gegenstand("Leichte Jacke", 1))
+    kleidung.gegenstand_hinzufuegen(Gegenstand("Unterwäsche", 5))
+
     technik = Kategorie("Technik")
     technik.gegenstand_hinzufuegen(Gegenstand("Handyladegerät"))
     technik.gegenstand_hinzufuegen(Gegenstand("Kamera", 1, gepackt=True))
+
+    accessoire = Kategorie("Accessoire")
+    accessoire.gegenstand_hinzufuegen(Gegenstand("Sonnenbrille", 1))
+
+    sonstiges = Kategorie("Sonstiges")
+    sonstiges.gegenstand_hinzufuegen(Gegenstand("Bahnticket", 1))
+    sonstiges.gegenstand_hinzufuegen(Gegenstand("Pass oder ID", 1))
+    sonstiges.gegenstand_hinzufuegen(Gegenstand("Geld ca. 500€", 1))
+    sonstiges.gegenstand_hinzufuegen(Gegenstand("Verpflegung für Unterwegs", 1))
+
     stadt.kategorie_hinzufuegen(kleidung)
     stadt.kategorie_hinzufuegen(technik)
+    stadt.kategorie_hinzufuegen(accessoire)
+    stadt.kategorie_hinzufuegen(sonstiges)
     reisen.append(stadt)
 
     # Beispiel 2 – Wanderurlaub
@@ -31,6 +44,8 @@ def beispielreisen_laden() -> (
     ausruestung = Kategorie("Ausrüstung")
     ausruestung.gegenstand_hinzufuegen(Gegenstand("Wanderschuhe"))
     ausruestung.gegenstand_hinzufuegen(Gegenstand("Rucksack"))
+    accessoire = Kategorie("Accessoire")
+    accessoire.gegenstand_hinzufuegen(Gegenstand("Sonnenbrille", 1))
     wandern.kategorie_hinzufuegen(ausruestung)
     reisen.append(wandern)
 
